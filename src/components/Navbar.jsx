a884c0c176ad61a3315cf42e2e0836380e716702
Max de Vocht <maxdevocht@hotmail.com>
2024-04-21 10:40:35 +0200

    Added styling

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav className="flex items-center border-black border border-solid rounded-full mx-5 my-3 p-3">
+            <div className="max-w-80 ml-10 flex items-center justify-between">
+                <a href=""><i className="ri-instagram-fill mr-7 text-3xl text-gray-950"></i></a>
+                <a href=""><i className="ri-github-fill mr-7 text-3xl text-gray-950"></i></a>
+                <a href=""><i className="ri-linkedin-box-fill mr-7 text-3xl text-gray-950"></i></a>
+                <a href=""><i className="ri-mail-fill text-3xl text-gray-950"></i></a>
+            </div>
+        </nav>
+    )
+}
