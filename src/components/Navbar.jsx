cead87ba1303379de76458c46475e370f170d375
Max de Vocht <maxdevocht@hotmail.com>
2024-04-22 16:11:11 +0200

    moved folder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Navbar() {
     return (
-        <nav className="flex items-center border-black border border-solid rounded-full mx-5 my-3 p-3 shadow-lg">
-            <div className="max-w-80 ml-10 flex items-center justify-between max-[835px]:flex max-[835px]:justify-center">
-                <a href="https://www.instagram.com/maxde.v/"><i className="ri-instagram-fill mr-7 text-3xl text-gray-950"></i></a>
-                <a href="https://github.com/maxdevocht"><i className="ri-github-fill mr-7 text-3xl text-gray-950"></i></a>
-                <a href="#"><i className="ri-linkedin-box-fill mr-7 text-3xl text-gray-950"></i></a>
+        <nav className="flex items-center border-black border border-solid rounded-full mx-5 my-3 p-3 shadow-lg max-[835px]:justify-center">
+            <div className="w-60 ml-10 flex flex-row items-center justify-between">
+                <a href="https://www.instagram.com/maxde.v/"><i className="ri-instagram-fill text-3xl text-gray-950"></i></a>
+                <a href="https://github.com/maxdevocht"><i className="ri-github-fill text-3xl text-gray-950"></i></a>
+                <a href="#"><i className="ri-linkedin-box-fill text-3xl text-gray-950"></i></a>
                 <a href="mailto:maxdevocht@hotmail.com"><i className="ri-mail-fill text-3xl text-gray-950"></i></a>
             </div>
         </nav>
