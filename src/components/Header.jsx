6ae4361e22cdc52ccdab7e5dd877d7b19f93804d
Max de Vocht <maxdevocht@hotmail.com>
2024-04-22 15:31:17 +0200

    Added responsive styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function Header() {
     return (
-        <header className="mx-5 my-3 flex flex-row gap-2">
-            <div className="min-h-52 w-1/2 flex flex-col items-start justify-center border-black border border-solid rounded-[30px] pl-14 shadow-lg">
-                <h1 className="text-6xl font-outfit font-bold">I'm Max 👋</h1>
-                <h2 className="font-outfit text-4xl font-light italic">Front-end Dev</h2>
+        <header className="mx-5 my-3 flex flex-row gap-2 max-[835px]:flex-col">
+            <div className="min-h-52 w-1/2 flex flex-col items-start justify-center border-black border border-solid rounded-[30px] pl-14 shadow-lg max-[835px]:w-full max-[835px]:text-center">
+                <h1 className="text-6xl font-outfit font-bold max-[835px]:text-5xl">I'm Max 👋</h1>
+                <h2 className="font-outfit text-4xl font-light italic max-[835px]:text-3xl">Front-end Dev</h2>
             </div>
 
-            <div className="min-h-52 w-1/2 flex items-center justify-center border-black border border-solid rounded-[30px] shadow-lg">
-                <img autoPlay muted loop src="./src/assets/images/memoji_gif2.gif" alt="memoji" className="object-cover w-80"/>
+            <div className="min-h-52 w-1/2 flex items-center justify-center border-black border border-solid rounded-[30px] shadow-lg max-[835px]:w-full">
+                <img autoPlay muted loop src="./src/assets/images/memoji_gif2.gif" alt="memoji" className="object-cover w-80 max-[835px]:w-60"/>
             </div>
         </header>
     )
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 export default function Links() {
     return (
-        <section className="mx-5 my-3 flex flex-col gap-1">
+        <section className="mx-5 my-3 flex flex-col gap-1 max-[1100px]:mb-12">
             <div className="w-full flex gap-1">
-                <a href="#" className="w-3/5 border-black border border-solid rounded-[30px] pl-14 shadow-lg text-[45px] font-outfit font-light italic tracking-wide hover:text-white hover:bg-black transition-all ease-in-out duration-75">IX UITLEENFORMULIER</a>
-                <a href="#" className="w-2/5 border-black border border-solid rounded-[30px] pl-14 shadow-lg text-[45px] font-outfit font-light italic tracking-wide hover:text-white hover:bg-black transition-all ease-in-out duration-75">BQ MUSIC</a>
+                <a href="#" className="w-3/5 border-black border border-solid rounded-[30px] pl-14 shadow-lg text-[45px] font-outfit font-light italic tracking-wide hover:text-white hover:bg-black transition-all ease-in-out duration-75 max-[1100px]:text-[30px] max-[1100px]:py-2 max-[835px]:text-[18px] max-[835px]:pl-6">IX UITLEENFORMULIER</a>
+                <a href="#" className="w-2/5 border-black border border-solid rounded-[30px] pl-14 shadow-lg text-[45px] font-outfit font-light italic tracking-wide hover:text-white hover:bg-black transition-all ease-in-out duration-75 max-[1100px]:text-[30px] max-[1100px]:py-2 max-[835px]:text-[18px] max-[835px]:pl-6">BQ MUSIC</a>
             </div>
             <div className="w-full flex gap-1">
-                <a href="#" className="w-1/2 border-black border border-solid rounded-[30px] pl-14 shadow-lg text-[45px] font-outfit font-light italic tracking-wide hover:text-white hover:bg-black transition-all ease-in-out duration-75">SSSYMBOLSSS</a>
-                <a href="https://mijnlijst.netlify.app" className="w-1/2 border-black border border-solid rounded-[30px] pl-14 shadow-lg text-[45px] font-outfit font-light italic tracking-wide hover:text-white hover:bg-black transition-all ease-in-out duration-75">MY SHOPPING LIST</a>
+                <a href="#" className="w-1/2 border-black border border-solid rounded-[30px] pl-14 shadow-lg text-[45px] font-outfit font-light italic tracking-wide hover:text-white hover:bg-black transition-all ease-in-out duration-75 max-[1100px]:text-[30px] max-[1100px]:py-2 max-[835px]:text-[18px] max-[835px]:pl-6">SSSYMBOLSSS</a>
+                <a href="https://mijnlijst.netlify.app" className="w-1/2 border-black border border-solid rounded-[30px] pl-14 shadow-lg text-[45px] font-outfit font-light italic tracking-wide hover:text-white hover:bg-black transition-all ease-in-out duration-75 max-[1100px]:text-[30px] max-[1100px]:py-2 max-[835px]:text-[18px] max-[835px]:pl-6">MY SHOPPING LIST</a>
             </div>
             <div className="w-full flex gap-1">
-                <a href="https://my-todo-checklist.netlify.app" className="w-2/5 border-black border border-solid rounded-[30px] pl-14 shadow-lg text-[45px] font-outfit font-light italic tracking-wide hover:text-white hover:bg-black transition-all ease-in-out duration-75">MY TODO APP</a>
-                <a href="#" className="w-3/5 border-black border border-solid rounded-[30px] pl-14 shadow-lg text-[45px] font-outfit font-light italic tracking-wide hover:text-white hover:bg-black transition-all ease-in-out duration-75">MEME GENERATOR</a>
+                <a href="https://my-todo-checklist.netlify.app" className="w-2/5 border-black border border-solid rounded-[30px] pl-14 shadow-lg text-[45px] font-outfit font-light italic tracking-wide hover:text-white hover:bg-black transition-all ease-in-out duration-75 max-[1100px]:text-[30px] max-[1100px]:py-2 max-[835px]:text-[18px] max-[835px]:pl-6">MY TODO APP</a>
+                <a href="#" className="w-3/5 border-black border border-solid rounded-[30px] pl-14 shadow-lg text-[45px] font-outfit font-light italic tracking-wide hover:text-white hover:bg-black transition-all ease-in-out duration-75 max-[1100px]:text-[30px] max-[1100px]:py-2 max-[835px]:text-[18px] max-[835px]:pl-6">MEME GENERATOR</a>
             </div>
         </section>
     )
