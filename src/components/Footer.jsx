a798668e10c7f9568396345b30252e7cf788271b
Max de Vocht <maxdevocht@hotmail.com>
2024-04-24 11:38:17 +0200

    chanmged bg color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <footer className="w-full mt-10 mb-[15px] flex items-end justify-center max-[1100px]:mb-[35px]">
+        <footer className=" w-full mt-10 mb-[15px] flex items-end justify-center max-[1100px]:mb-[35px]">
             <p className="font-outfit font-light"><i class="ri-copyright-fill"></i> 2024 Max de Vocht. All Rights Reserved</p>
         </footer>
     )
