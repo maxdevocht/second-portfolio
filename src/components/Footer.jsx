53a57d6021ab56754daaa06dca94e325f7ceb3e2
Max de Vocht <maxdevocht@hotmail.com>
2024-06-06 14:13:40 +0200

    Update Links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,6 @@
-import React from "react";
-
 export default function Footer() {
     return (
-        <footer className=" w-full mb-4 flex items-end justify-center">
+        <footer className="flex w-full mb-4 justify-center">
             <p className="font-outfit font-light"><i class="ri-copyright-fill"></i> 2024 Max de Vocht. All Rights Reserved</p>
         </footer>
     )
